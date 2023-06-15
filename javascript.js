cab4a9e1ccfa597b3cd2df93dc4e28952aaf683f
Asbelll <chrystian_alex@hotmail.com>
2023-06-15 18:20:25 -0300

    Adiciona botão Enter como trigger para submit
    Retira valor do input ao completar submit

--- javascript.js
+++ javascript.js
@@ -6,6 +6,18 @@ $(document).ready(function() {
   $('#submitDia').click(function() {
       submitInput("#tarefaDia", "#listaDia")
   })
+
+  $("#tarefaNoite").on('keyup', function (e) {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+      submitInput("#tarefaNoite", "#listaNoite")
+    }
+  })
+
+  $("#tarefaDia").on('keyup', function (e) {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+      submitInput("#tarefaDia", "#listaDia")
+    }
+  });
 })
 
 $(document).on('click', '#remove', function() {
@@ -17,5 +29,6 @@ function submitInput(source, target) {
 
   if (data !== '') {
     $(target).append('<li class="list-group-item d-flex justify-content-between"><div class="px-2 flex-grow-1">' + data + '</div><button class="btn btn-outline-secondary btn-sm" id="remove"><i class="fa-solid fa-x pt-1"></i></button></li>')
+    $(source).val('')
   }
 }
